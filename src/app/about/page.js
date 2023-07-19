e7d73db763fcb184418c8ca23c1c620f99458dec
HowToBasicDD <benefit5758@naver.com>
2023-07-19 17:01:22 +0900

    4. use client의 사용방법 / 컴포넌트화 방법 익히기 필요

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,7 +1,32 @@
+"use client";
+import { useState } from "react";
+
 export default function about() {
+  const [count, SetCount] = useState(0);
   return (
     <>
-      <div>hello, 제 이름은 이승빈 입니다.</div>
+      <div>
+        hello, 제 이름은 이승빈 입니다. {count}
+        <button
+          onClick={() => {
+            SetCount(count + 1);
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            SetCount(count - 1);
+          }}
+        >
+          -
+        </button>
+        <p>
+          폴더구조와 page.js의 기본 이해 폴더는 곧 url의 연장, page.js는 그
+          url의 얼굴 폴더내에서 컴포넌트화해서 사용하되, 각각의 컴포넌트는 다른
+          폴더에 저장?
+        </p>
+      </div>
     </>
   );
 }
