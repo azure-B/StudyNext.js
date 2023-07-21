8f96198e60f17f38681bd6fb54fcb6b987e392f1
HowToBasicDD <benefit5758@naver.com>
2023-07-21 10:58:14 +0900

    6. Link에 Style넣을 때 유의사항

--- components/NevBar/NevBar.js
+++ components/NevBar/NevBar.js
@@ -4,7 +4,11 @@ export default function NevBar() {
   return (
     <>
       <div>
-        <Link href="/">HOME</Link>
+        <Link href="/">
+          <p>hello</p>
+          {/* Anchor태그가 안들어 갈 뿐, 나머지 태그를 넣었을 때에는 여전히 동작한다. 
+         Link에는 스타일이 안들어가므로 참고하자 */}
+        </Link>
       </div>
       <div>
         <Link href="/about">ABOUT</Link>
